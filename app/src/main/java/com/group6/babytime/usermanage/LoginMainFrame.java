10466293003fe7d0c42e539c604535b7606da081
user <690948601@qq.com>
2016-10-11 14:59:13 +0800

    author:陈歆宇 desc:实现了第三方微博登录功能

--- app/src/main/java/com/group6/babytime/usermanage/LoginMainFrame.java
+++ app/src/main/java/com/group6/babytime/usermanage/LoginMainFrame.java
@@ -3,9 +3,11 @@ package com.group6.babytime.usermanage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.group6.babytime.R;
 import com.group6.babytime.constant.APPConstants;
@@ -13,6 +15,9 @@ import com.sina.weibo.sdk.auth.AuthInfo;
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.auth.WeiboAuthListener;
 import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
+
+import java.text.SimpleDateFormat;
 
 public class LoginMainFrame extends AppCompatActivity {
 
@@ -32,7 +37,6 @@ public class LoginMainFrame extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login_main_frame);
 
         login_btn = (Button) findViewById(R.id.login);
@@ -42,6 +46,9 @@ public class LoginMainFrame extends AppCompatActivity {
         weibo_btn = ((Button) findViewById(R.id.weibo_btn));
         weixin_btn = ((Button) findViewById(R.id.weixin_btn));
 
+        //微博初始化
+        mAuthInfo = new AuthInfo(this, APPConstants.WEIBO_APP_KEY, APPConstants.SINA_REDIRECT_URL, APPConstants.SINA_SCOPE);
+        mSsoHandler = new SsoHandler(this, mAuthInfo);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,8 +66,84 @@ public class LoginMainFrame extends AppCompatActivity {
             }
         });
 
+        weibo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSsoHandler.authorize(new AuthListener());
+            }
+        });
+
+
 
     }
+    class AuthListener implements WeiboAuthListener {
 
+        @Override
+        public void onComplete(Bundle values) {
+            // 从 Bundle 中解析 Token
+            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+            //从这里获取用户输入的 电话号码信息
+            String  phoneNum =  mAccessToken.getPhoneNum();
+            if (mAccessToken.isSessionValid()) {
+                // 显示 Token
+                updateTokenView(false);
 
+                // 保存 Token 到 SharedPreferences
+                AccessTokenKeeper.writeAccessToken(LoginMainFrame.this, mAccessToken);
+                Toast.makeText(LoginMainFrame.this,
+                        R.string.weibosdk_demo_toast_auth_success, Toast.LENGTH_SHORT).show();
+            } else {
+                // 以下几种情况，您会收到 Code：
+                // 1. 当您未在平台上注册的应用程序的包名与签名时；
+                // 2. 当您注册的应用程序包名与签名不正确时；
+                // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
+                String code = values.getString("code");
+                String message = getString(R.string.weibosdk_demo_toast_auth_failed);
+                if (!TextUtils.isEmpty(code)) {
+                    message = message + "\nObtained the code: " + code;
+                }
+                Toast.makeText(LoginMainFrame.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(LoginMainFrame.this,
+                    R.string.weibosdk_demo_toast_auth_canceled, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+            Toast.makeText(LoginMainFrame.this,
+                    "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 显示当前 Token 信息。
+     *
+     * @param hasExisted 配置文件中是否已存在 token 信息并且合法
+     */
+    private void updateTokenView(boolean hasExisted) {
+        String date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(
+                new java.util.Date(mAccessToken.getExpiresTime()));
+        String format = getString(R.string.weibosdk_demo_token_to_string_format_1);
+
+        String message = String.format(format, mAccessToken.getToken(), date);
+        if (hasExisted) {
+            message = getString(R.string.weibosdk_demo_token_has_existed) + "\n" + message;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // SSO 授权回调
+        // 重要：发起 SSO 登陆的 Activity 必须重写 onActivityResults
+        if (mSsoHandler != null) {
+            mSsoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+
+    }
 }

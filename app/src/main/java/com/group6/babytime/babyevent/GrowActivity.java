0af2960eed9f40d06f349fbf2612cefa9a4c6ae6
cxy950705 <690948601@qq.com>
2016-10-20 18:45:47 +0800

    顾宁博 宝宝事件的主框架

---
+++ app/src/main/java/com/group6/babytime/babyevent/GrowActivity.java
@@ -0,0 +1,15 @@
+package com.group6.babytime.babyevent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.group6.babytime.R;
+
+public class GrowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grow);
+    }
+}

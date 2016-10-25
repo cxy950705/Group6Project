9e6fbcad84c88b8083422214311d193095029849
XZJ1993 <735539096@qq.com>
2016-10-25 09:11:46 +0800

    author:薛志杰 全部项目第n次合并

--- app/src/main/java/com/group6/babytime/utils/NetUtil.java
+++ app/src/main/java/com/group6/babytime/utils/NetUtil.java
@@ -4,5 +4,5 @@ package com.group6.babytime.utils;
  * Created by Administrator on 2016/10/18.
  */
 public class NetUtil {
-    public static final String url="http://10.0.2.2:8080/babytimeweb/";
+    public static final String url="http://10.40.5.9:8080/babytimeweb/";
 }

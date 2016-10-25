70f7c562252255990396de5db5162d8ae04ccc69
cxy950705 <690948601@qq.com>
2016-10-25 21:46:50 +0800

    author:顾宁博，卢海龙 dec：完善界面，添加细小功能点

--- app/src/main/java/com/group6/babytime/babyevent/ErGeFragment.java
+++ app/src/main/java/com/group6/babytime/babyevent/ErGeFragment.java
@@ -1,6 +1,8 @@
 package com.group6.babytime.babyevent;
 
+
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,20 +11,22 @@ import android.view.ViewGroup;
 import com.group6.babytime.R;
 
 
+
+
+
 /**
  * Created by Administrator on 2016/10/21 0021.
  */
 public class ErGeFragment extends Fragment {
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         super.onCreateView(inflater, container, savedInstanceState);
         View ergeView = inflater.inflate(R.layout.ctivity_tab_erge, container,false);
+
         return ergeView;
 
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState){
-        super.onActivityCreated(savedInstanceState);
-    }
 }
